ee5970956bab96e45f8f010af9c860c2099caeaa
Randy Tellez Galan <kronhyx@gmail.com>
2021-04-20 18:36:39 -0300

    perf(test): reject ssl verification in axios

--- src/plugin/axios.ts
+++ src/plugin/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
+import * as https from 'https'
 
 axios.defaults.baseURL = process.env.baseUrl
+axios.defaults.httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+})
 
 export default axios
