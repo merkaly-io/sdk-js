d6c10cfcb0a65c59da9196cb6466f087636a5336
Randy Tellez Galan <kronhyx@gmail.com>
2021-04-21 02:01:03 -0300

    feat(test): add product jest test

--- src/plugin/axios.ts
+++ src/plugin/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import * as https from 'https'
 
 axios.defaults.baseURL = process.env.baseUrl
+axios.defaults.headers['Authorization'] = `Bearer ${process.env.accessToken}`
 axios.defaults.httpsAgent = new https.Agent({
   rejectUnauthorized: false
 })
