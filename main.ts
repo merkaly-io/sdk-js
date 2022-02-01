997cf1bf454dcd4535f27187b2a05613629b2060
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-01 01:51:50 -0300

    chore: add sdk nuxt module

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Module } from '@nuxt/types'
 const MerkalySDKModule: Module = function () {
   const { options } = this
 
-  options.plugins.push('@/plugins/sdk')
+  options.plugins.push({ src: '@/plugins/sdk' })
 }
 
 export default MerkalySDKModule
--- plugins/sdk.ts
+++ plugins/sdk.ts
@@ -1,15 +1,21 @@
 import { Context } from '@nuxt/types'
 import { ManagerSDK } from '@/src'
 
-declare module 'vue/types/vue' {
-  interface Vue {
+declare module '@nuxt/types' {
+  interface Context {
     $merkaly: ManagerSDK
+  }
+
+  interface NuxtAppOptions {
+    $merkaly: ManagerSDK
+  }
+
+  interface Configuration {
+    merkaly?: ManagerSDK
   }
 }
 
 export default ({ app, $config }: Context, inject: Function) => {
   app.$merkaly = new ManagerSDK($config.merkaly?.baseUrl)
   inject('merkaly', app.$merkaly)
-
-  return app.$merkaly
 }
