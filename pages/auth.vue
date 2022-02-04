d515bca53a362930746478285d72af201f766011
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-04 11:38:21 -0300

    perf: add auth page

---
+++ pages/auth.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>auth page</div>
+</template>
+
+<script>
+export default {
+  name: 'AuthLogin'
+}
+</script>
+
+<style scoped>
+
+</style>
--- store/index.ts
+++ store/index.ts
@@ -1,1 +1,9 @@
-export const state = () => ({})
+import { Auth } from '@nuxtjs/auth-next'
+
+export default interface Store {
+  auth: Auth
+}
+
+export const state = (): Partial<Store> => ({})
+
+export const actions = {}
