c0398b7af983a9e02d4aa115e8366bfed1b9d907
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-05 02:18:11 -0300

    refactor: remove auth from merkaly and use direct from plugin instead

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,12 +1,9 @@
 import { NuxtAppOptions } from '@nuxt/types'
-import { Auth } from '@nuxtjs/auth-next'
 
 export default abstract class MerkalySDK {
   public readonly $app: NuxtAppOptions
-  public readonly auth: Auth
 
   constructor ($app: NuxtAppOptions) {
     this.$app = $app
-    this.auth = this.$app.$auth
   }
 }
