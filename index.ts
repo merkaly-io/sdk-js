b158f303bbd6c9de57ece0a39db17faf7bba398d
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-05 02:14:36 -0300

    perf: change type definition of user

--- index.ts
+++ index.ts
@@ -1,8 +1,33 @@
 import { Context } from '@nuxt/types'
+import { Auth as NuxtAuth } from '@nuxtjs/auth-next'
+import { UserData } from 'auth0'
 import './plugins/axios'
 
 declare global {
   const $nuxt: Context
+}
+
+export interface User {
+  id: string
+  name: string
+}
+
+declare module 'vue/types/vue' {
+  interface Auth extends NuxtAuth {
+    user: UserData & typeof NuxtAuth.prototype.user
+  }
+}
+
+declare module '@nuxt/types' {
+  interface Auth extends NuxtAuth {
+    user: UserData & typeof NuxtAuth.prototype.user
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface Auth extends NuxtAuth {
+    user: UserData & typeof NuxtAuth.prototype.user
+  }
 }
 
 export { AdminSDK } from './src/sdk.admin'
