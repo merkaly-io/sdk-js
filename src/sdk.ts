d8d5c954254b4769e5268475b0ca8032f6ec9b01
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-05 02:16:13 -0300

    chore: using public instead private

--- src/sdk.ts
+++ src/sdk.ts
@@ -2,7 +2,7 @@ import { NuxtAppOptions } from '@nuxt/types'
 import { Auth } from '@nuxtjs/auth-next'
 
 export default abstract class MerkalySDK {
-  private readonly $app: NuxtAppOptions
+  public readonly $app: NuxtAppOptions
   public readonly auth: Auth
 
   constructor ($app: NuxtAppOptions) {
