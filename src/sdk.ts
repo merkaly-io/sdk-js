3e24b5ffc97d9a2908bdb5111df1f862a9f195ea
Randy Tellez Galán <kronhyx@gmail.com>
2022-02-06 15:58:37 -0300

    refactor: remove constructor for SDK

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,9 +1,2 @@
-import { NuxtAppOptions } from '@nuxt/types'
-
 export default abstract class MerkalySDK {
-  public readonly $app: NuxtAppOptions
-
-  constructor ($app: NuxtAppOptions) {
-    this.$app = $app
-  }
 }
