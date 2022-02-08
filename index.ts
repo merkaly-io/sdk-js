2dbde72160953a319767a69e8be9f5d9355a011a
Randy Tellez Galan <kronhyx@gmail.com>
2022-02-07 22:35:07 -0300

    refactor: moving declarations to index entry point

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Context } from '@nuxt/types'
 import { Auth as NuxtAuth } from '@nuxtjs/auth-next'
+import { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { UserData } from 'auth0'
 
 declare global {
@@ -26,6 +27,32 @@ declare module '@nuxt/types' {
 declare module 'vuex/types/index' {
   interface Auth extends NuxtAuth {
     user: UserData & typeof NuxtAuth.prototype.user
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $api: NuxtAxiosInstance
+  }
+}
+
+declare module '@nuxt/types' {
+  interface Context {
+    $api: NuxtAxiosInstance
+  }
+
+  interface NuxtAppOptions {
+    $api: NuxtAxiosInstance
+  }
+
+  interface Configuration {
+    api?: NuxtAxiosInstance
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $api: NuxtAxiosInstance
   }
 }
 
--- plugins/plugin.axios.ts
+++ plugins/plugin.axios.ts
@@ -1,31 +1,4 @@
 import { Plugin } from '@nuxt/types'
-import { NuxtAxiosInstance } from '@nuxtjs/axios'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $api: NuxtAxiosInstance
-  }
-}
-
-declare module '@nuxt/types' {
-  interface Context {
-    $api: NuxtAxiosInstance
-  }
-
-  interface NuxtAppOptions {
-    $api: NuxtAxiosInstance
-  }
-
-  interface Configuration {
-    api?: NuxtAxiosInstance
-  }
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $api: NuxtAxiosInstance
-  }
-}
 
 const plugin: Plugin = ({ app, $axios, $config }, inject) => {
   const api = $axios.create()
