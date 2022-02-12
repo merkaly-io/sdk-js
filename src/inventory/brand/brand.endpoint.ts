133f33942ffed468adc6d6b353d66701ad3919d7
Randy Tellez Galan <kronhyx@gmail.com>
2022-02-12 11:22:27 -0300

    perf: add class transformer to inventory brand

--- src/inventory/brand/brand.endpoint.ts
+++ src/inventory/brand/brand.endpoint.ts
@@ -1,29 +1,34 @@
 import { join } from 'path'
-import { $InventoryPath } from '@merkaly/api/src/inventory'
+import { plainToInstance } from 'class-transformer'
+import { $InventoryPath } from '@merkaly/api/src/inventory/'
 import { BrandEntity, CreateBrandValidator, UpdateBrandValidator } from '@merkaly/api/src/inventory/brands'
 import BrandReference from './brand.reference'
 
 export const route = (...path: string[]) => join($InventoryPath, BrandEntity.$path, ...path)
 
 namespace Brand {
-  export const find = (): Promise<BrandReference[]> => {
-    return $nuxt.$api.$get(route())
+  export const find = () => {
+    return $nuxt.$api.$get<BrandReference[]>(route())
+      .then(brands => brands.map(brand => plainToInstance(BrandReference, brand)))
   }
 
-  export const read = (id: string): Promise<BrandReference> => {
-    return $nuxt.$api.$get(route(id))
+  export const read = (id: string) => {
+    return $nuxt.$api.$get<BrandReference>(route(id))
+      .then(brand => plainToInstance(BrandReference, brand))
   }
 
-  export const create = (payload: CreateBrandValidator): Promise<BrandReference> => {
-    return $nuxt.$api.$post(route(), payload)
+  export const create = (payload: CreateBrandValidator) => {
+    return $nuxt.$api.$post<BrandReference>(route(), payload)
+      .then(brand => plainToInstance(BrandReference, brand))
   }
 
-  export const update = (id: string, payload: UpdateBrandValidator): Promise<BrandReference> => {
-    return $nuxt.$api.$put(route(id), payload)
+  export const update = (id: string, payload: UpdateBrandValidator) => {
+    return $nuxt.$api.$put<BrandReference>(route(id), payload)
+      .then(brand => plainToInstance(BrandReference, brand))
   }
 
-  export const remove = (id: string): Promise<void> => {
-    return $nuxt.$api.$delete(route(id))
+  export const remove = (id: string) => {
+    return $nuxt.$api.$delete<void>(route(id))
   }
 }
 
