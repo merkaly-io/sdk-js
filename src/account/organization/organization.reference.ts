70b392f8c1d6a83513ce30a2c22dd3abb5752641
Randy Tellez Galan <kronhyx@gmail.com>
2022-03-03 01:28:05 -0300

    refactor: add method to retrieve connections from organization

--- src/account/organization/organization.reference.ts
+++ src/account/organization/organization.reference.ts
@@ -3,7 +3,7 @@ import {
   RemoveOrganizationMembers
 } from '@merkaly/api/src/account/organizations/members/member.validator'
 import { IdOrganizationValidator } from '@merkaly/api/src/account/organizations/organization.validator'
-import { Organization, OrganizationMember } from 'auth0'
+import { Organization, OrganizationMember, OrganizationConnection } from 'auth0'
 
 export default class OrganizationReference implements Organization {
   public id: IdOrganizationValidator
@@ -11,6 +11,7 @@ export default class OrganizationReference implements Organization {
   public display_name: Organization['display_name']
   public branding: Organization['branding']
   public members: OrganizationMember[] = []
+  public connections: OrganizationConnection[] = []
 
   public getMembers () {
     return $nuxt.$api.$get<OrganizationMember[]>('/account/organizations/' + this.id + '/members/')
@@ -25,5 +26,10 @@ export default class OrganizationReference implements Organization {
   public removeMembers (ids: RemoveOrganizationMembers) {
     return $nuxt.$api.$delete<OrganizationMember[]>('/account/organizations/' + this.id + '/members/', { data: ids })
       .then(members => (this.members = members))
+  }
+
+  public getConnections () {
+    return $nuxt.$api.$get<OrganizationConnection[]>('/account/organizations/' + this.id + '/connections/')
+      .then(connections => (this.connections = connections))
   }
 }
