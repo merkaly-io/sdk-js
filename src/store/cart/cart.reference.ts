c296dd4c60a82bebfe230ecf6983e2ed32f93112
Randy Tellez Galan <kronhyx@gmail.com>
2022-03-02 21:52:11 -0300

    refactor!: add new params to endpoint find method

--- src/store/cart/cart.reference.ts
+++ src/store/cart/cart.reference.ts
@@ -1,4 +1,4 @@
-import { CartEntity } from '@merkaly/api/src/store/carts'
+import { CartEntity } from '@merkaly/api/src/store/carts/cart.entity'
 
 export default class CartReference extends CartEntity {
 }
