56e063ad5ba7436b4b0d0294d0abdc9b187ae20e
Randy Tellez Galan <kronhyx@gmail.com>
2022-03-04 04:22:44 -0300

    ci: change node engine version

--- plugins/plugin.axios.ts
+++ plugins/plugin.axios.ts
@@ -2,10 +2,11 @@ import { Plugin } from '@nuxt/types'
 import MerkalySDK from '@merkaly/sdk-js/src/sdk'
 
 const plugin: Plugin = ({ $axios, $config, req }) => {
-  let baseURL = `/${$config.merkaly.proxy}`
+  let baseURL = `/${$config.merkaly.proxy}/`
 
   if (process.server) {
     baseURL = (req.headers.host) + baseURL
+    baseURL = 'http://' + baseURL
   }
 
   MerkalySDK.prototype.$axios = $axios.create({ baseURL })
