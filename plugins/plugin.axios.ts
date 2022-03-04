64d11b333aa98269a0d8f3e25733babb22fd6dd0
Randy Tellez Galan <kronhyx@gmail.com>
2022-03-04 03:47:09 -0300

    chore: add path resolver

--- plugins/plugin.axios.ts
+++ plugins/plugin.axios.ts
@@ -1,9 +1,9 @@
 import { Plugin } from '@nuxt/types'
-import MerkalySDK from '../src/sdk'
+import MerkalySDK from '@merkaly/sdk-js/src/sdk'
 
 const plugin: Plugin = ({ $axios, $config }) => {
   MerkalySDK.prototype.$axios = $axios.create({
-    baseURL: $config.merkaly.proxy
+    baseURL: `/${$config.merkaly.proxy}`
   })
 }
 
