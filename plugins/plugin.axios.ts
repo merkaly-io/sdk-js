858324d2611de701b25a5c0d6195337e0452237d
Randy Tellez Galan <kronhyx@gmail.com>
2022-03-04 04:17:04 -0300

    fix: error on call sdk from server side

--- plugins/plugin.axios.ts
+++ plugins/plugin.axios.ts
@@ -1,10 +1,14 @@
 import { Plugin } from '@nuxt/types'
 import MerkalySDK from '@merkaly/sdk-js/src/sdk'
 
-const plugin: Plugin = ({ $axios, $config }) => {
-  MerkalySDK.prototype.$axios = $axios.create({
-    baseURL: `/${$config.merkaly.proxy}`
-  })
+const plugin: Plugin = ({ $axios, $config, req }) => {
+  let baseURL = `/${$config.merkaly.proxy}`
+
+  if (process.server) {
+    baseURL = (req.headers.host) + baseURL
+  }
+
+  MerkalySDK.prototype.$axios = $axios.create({ baseURL })
 }
 
 export default plugin
